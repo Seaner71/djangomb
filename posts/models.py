ae4a2fe693a601d0d3e896723b8c901239885964
echo 'hello world <ssmyth1071@gmail.com>
2018-02-01 09:08:44 -0500

    add test model from localstove

--- posts/models.py
+++ posts/models.py
@@ -6,3 +6,11 @@ class Post(models.Model):
     def __str__(self):
         """A string representation of the model"""
         return self.text[:50]
+class Dish(models.Model):
+    name = models.CharField(max_length=4000, blank=False)
+    description = models.CharField(max_length=4000, blank=False)
+    price = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        """A string representation of the model"""
+        return self.name[:50]
